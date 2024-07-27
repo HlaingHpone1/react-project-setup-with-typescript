ab962f9a17eaf072aec6047998b1ea01fd9618b5
HlaingHpone1 <hlainghpone1@gmail.com>
2024-07-27 19:11:01 +0630

    Add Project Setup With TypeScript

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 2, // Data is fresh for 2 minutes
+      // refetchIntervalInBackground: true,
+    }
+  }
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </BrowserRouter>
+
+)
