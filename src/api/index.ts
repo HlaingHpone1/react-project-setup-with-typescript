7c0f31d56a919092a6ca4753e8470689fd556ce2
hlainghpone2003 <hlainghpone@aceplussolutions.com>
2025-05-07 16:22:41 +0630

    refactor and update project structure

---
+++ src/api/index.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const baseURL = import.meta.env.VITE_BASE_URL + '/api';
+
+const api = axios.create({
+	baseURL: baseURL,
+	headers: {
+		ContentType: 'application/json',
+		'Cache-Control': 'no-cache',
+		'Access-Control-Allow-Origin': '*',
+		Accept: 'application/json',
+	},
+	withCredentials: true,
+});
+
+export default api;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,24 +1,5 @@
-import Navbar from './Navbar'
-import Footer from './Footer'
-import { Outlet } from 'react-router-dom'
-
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-
-
 const MainLayout = () => {
-  return (
-    <>
+	return <div>MainLayout</div>;
+};
 
-      <Navbar />
-      <Outlet />
-      <Footer />
-      {/* U can open the React Query Devtools */}
-      <ReactQueryDevtools />
-
-    </>
-  )
-}
-
-export default MainLayout
+export default MainLayout;
