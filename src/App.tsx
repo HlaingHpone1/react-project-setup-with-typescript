d59f2e82d918930cfb64bc56e315697581ecb005
hlainghpone1 <hlainghpone1@gmail.com>
2025-05-06 17:31:06 -0700

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import Router from "./router/Router"
-
-
 function App() {
-
-  return (
-    <>
-      <Router />
-    </>
-  )
+	return (
+		<>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+				Tempore debitis blanditiis cupiditat e expedita! Nisi, alias
+				dolores odio ab eum corrupti eaque ipsam, excepturi dicta
+				maiores maxime commodi. Providen t, officiis tempora!
+			</p>
+			THis is a test
+		</>
+	);
 }
 
-export default App
+export default App;
