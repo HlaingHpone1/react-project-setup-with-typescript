9d795aa763ee4c1d546f8f8e31568f47cccea66e
hlainghpone1 <hlainghpone1@gmail.com>
2025-05-10 00:40:50 -0700

    Refactor code structure for improved readability and maintainability

--- eslint.config.js
+++ eslint.config.js
@@ -4,33 +4,46 @@ import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import pluginQuery from '@tanstack/eslint-plugin-query';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 export default tseslint.config(
-	{ ignores: ['dist'] },
-	{
-		extends: [
-			js.configs.recommended,
-			...tseslint.configs.recommended,
-			...pluginQuery.configs['flat/recommended'],
-		],
-		files: ['**/*.{ts,tsx}'],
-		languageOptions: {
-			ecmaVersion: 2020,
-			globals: globals.browser,
-		},
-		plugins: {
-			'react-hooks': reactHooks,
-			'react-refresh': reactRefresh,
-		},
-		rules: {
-			...reactHooks.configs.recommended.rules,
-			'react-refresh/only-export-components': [
-				'warn',
-				{ allowConstantExport: true },
-			],
-			'no-unused-vars': ['warn'],
-			'@typescript-eslint/no-unused-vars': ['warn'],
-			'@typescript-eslint/no-explicit-any': 'error',
-		},
-	}
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...pluginQuery.configs['flat/recommended'],
+      eslintConfigPrettier,
+      importPlugin.flatConfigs.recommended,
+      eslintPluginUnicorn.configs.recommended,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      prettier,
+      'simple-import-sort': simpleImportSort,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-unused-vars': ['warn'],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unicorn/better-regex': 'warn',
+    },
+  }
 );
